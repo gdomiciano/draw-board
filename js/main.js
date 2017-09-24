b178eaa59529e750f48840e1f900cce24ef1a709
Geisy Domiciano <geisydomiciano@gmail.com>
2017-09-24 14:21:15 -0300

    finish sidebar

--- js/main.js
+++ js/main.js
@@ -1,66 +1,81 @@
 const board = document.querySelector('.Board');
+const resetButton = document.querySelector('.Reset');
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 const blue = '#005293';
 const yellow = '#FECB00';
 const pointRadius = 11;
 const pointColor = '#FF0000';
-let points = [];
-// let points = [
-//     {x: 6, y: 7},
-//     {x: 18, y: 26},
-//     {x: 48, y: 26},
-//     {x: 36, y: 7},
-// ];
+const info = {
+    points: []
+};
+let parallelogramInfo = {};
+let circleInfo = {};
 
 canvas.width = board.clientWidth || board.offsetWidth;
 canvas.height = board.clientHeight || board.offsetHeight;
 
+function resetBoard() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    info.points.length = 0;
+    canvas.addEventListener('mousedown', handleMouseClick);
+}
 
-function handleMouseClick(e) {
-    // console.log(e, document.body.scrollLeft, document.documentElement.scrollLeft)
+const  handleMouseClick = (e) => {
     let x = e.pageX || e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
     let y = e.pageY || e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
 
     x -= canvas.offsetLeft;
     y -= canvas.offsetTop;
 
-    console.log(x,y);
-    points.push({
+    info.points.push({
         x: x,
         y: y
     });
-    // console.log(points);
+
     drawPoint(x, y, pointRadius, pointColor);
-    if (points.length === 3) {
-        drawParallelogram(points);
+    context.fillText(info.points.length, x, y-25, 80);
+    if (info.points.length === 3) {
+        console.log(info);
+        drawParallelogram(info.points);
         canvas.removeEventListener('mousedown', handleMouseClick);
     }
 }
 
 canvas.addEventListener('mousedown', handleMouseClick);
+resetButton.addEventListener('mousedown', resetBoard);
 
 function drawParallelogram(points) {
+    console.log(points)
+    points.push({
+        x: points[0].x + points[2].x -points[1].x,
+        y: points[0].y + points[2].y -points[1].y,
+    });
+    const centerX = (points[0].x + points[1].x +points[2].x + points[3].x) / 4;
+    const centerY = (points[0].y + points[1].y +points[2].y + points[3].y) / 4;
+    // const // base = Math.sqrt(Math.pow((points[1].x - points[0].x),2)+Math.pow((points[1].y - [points[0].y]),2));
+    // const // base = Math.sqrt(Math.pow((points[3].x - points[0].x),2)+Math.pow((points[3].y - [points[0].y]),2));
+    const base = Math.max(Math.sqrt(Math.pow((points[3].x - points[0].x), 2)+Math.pow((points[3].y - [points[0].y]), 2)), Math.sqrt(Math.pow((points[1].x - points[0].x), 2)+Math.pow((points[1].y - [points[0].y]), 2)));
+    const AC = Math.sqrt(Math.pow((points[3].x - points[0].x),2)+Math.pow((points[3].y - [points[0].y]),2));
+    const BD = Math.sqrt(Math.pow((points[2].x - points[1].x),2)+Math.pow((points[2].y - [points[1].y]),2));
+    const area = (AC * BD)/2;
+    const height = area/base;
+    const radius = (height/2).toFixed();
+
     context.strokeStyle = blue;
     context.beginPath();
     context.moveTo(points[0].x, points[0].y);
     context.lineTo(points[1].x, points[1].y);
     context.lineTo(points[2].x, points[2].y);
-    points.push({
-        x: points[0].x + points[2].x -points[1].x,
-        y: points[0].y + points[2].y -points[1].y,
-    })
     context.lineTo(points[3].x, points[3].y);
     context.closePath();
     context.lineWidth=3;
     context.stroke();
-    let centerX = (points[0].x + points[1].x +points[2].x + points[3].x) / 4
-    let centerY = (points[0].y + points[1].y +points[2].y + points[3].y) / 4
 
-    let base = Math.sqrt(Math.pow((points[3].x - points[0].x),2)+Math.pow((points[3].y - [points[0].y]),2));
-    let height = (points[0].y - points[1].y);
-    let radius = height/2;
-    let area = height*base;
+    info.parallelogram = {
+        area: area.toFixed(),
+    }
+    // console.log(area);
     drawCircle(centerX, centerY, radius, yellow);
 }
 
@@ -69,6 +84,11 @@ function drawCircle(cx, cy, radius, color) {
     context.beginPath();
     context.arc(cx, cy, radius, 0, Math.PI * 2, true);
     context.stroke();
+    console.log(radius);
+    info.circle = {
+        area: (Math.PI*Math.pow(radius,2)).toFixed(),
+    };
+    showInfo(info);
 }
 
 function drawPoint(cx, cy, radius, color) {
@@ -77,10 +97,14 @@ function drawPoint(cx, cy, radius, color) {
     context.arc(cx, cy, radius, 0, Math.PI * 2, true);
     context.fill();
 }
-// drawParallelogram(points);
-console.log(points);
-function reset() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    points.length = 0;
-    canvas.addEventListener('mousedown', handleMouseClick);
+
+function showInfo(info){
+    console.log(info)
+    document.querySelector('.Info-content--pointOne').textContent = `${info.points[0].x}, ${info.points[0].y}`;
+    document.querySelector('.Info-content--pointTwo').textContent = `${info.points[1].x}, ${info.points[1].y}`;
+    document.querySelector('.Info-content--pointThree').textContent = `${info.points[2].x}, ${info.points[2].y}`;
+    document.querySelector('.Info-content--parallelogramArea').textContent += ` ${info.parallelogram.area}`;
+    document.querySelector('.Info-content--circleArea').textContent += ` ${info.circle.area}`;
+    document.querySelector('.Info-none').classList.add('u-hidden');
+    document.querySelector('.Info-content').classList.remove('u-hidden');
 }
